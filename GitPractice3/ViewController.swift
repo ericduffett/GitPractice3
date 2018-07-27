fef4d909b8c3b685e4b46db8b52a33d83e3642e8
Eric Duffett <duffette@nths.net>
2018-07-27 08:53:23 -0500

    Initial Commit

---
+++ GitPractice3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractice3
+//
+//  Created by Duffett, Eric on 7/27/18.
+//  Copyright © 2018 Undaunted Athlete, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
